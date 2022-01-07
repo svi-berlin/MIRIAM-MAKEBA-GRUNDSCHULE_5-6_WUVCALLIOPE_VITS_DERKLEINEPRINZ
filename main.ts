5b827ee97b78e27feed375da7bc36c57ebfa8bbb
svi-berlin <svi-berlin@users.noreply.github.com>
2022-01-07 07:20:02 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,24 @@
-basic.setLedColor(0xffff00)
-basic.showIcon(IconNames.Heart)
-basic.pause(500)
-basic.showIcon(IconNames.SmallHeart)
-basic.pause(500)
-basic.showIcon(IconNames.Heart)
-basic.pause(500)
-basic.showIcon(IconNames.SmallHeart)
-basic.setLedColor(0x00ff00)
-basic.forever(function () {
+function derKleinePrinz () {
     basic.showString("Alle großen Leute waren einmal Kinder, aber nur wenige erinnern sich daran.\"")
     basic.pause(500)
     basic.showIcon(IconNames.Surprised)
     basic.pause(500)
     basic.showIcon(IconNames.Happy)
     basic.pause(1000)
+}
+function blinkendesHerz () {
+    basic.setLedColor(0xffff00)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(500)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.setLedColor(0x00ff00)
+}
+blinkendesHerz()
+derKleinePrinz()
+basic.forever(function () {
+	
 })
